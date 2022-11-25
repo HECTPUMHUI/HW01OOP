37f624ad050fb812297968c1a1ae424329bc7de8
hectpumhui <forsoctask@gmail.com>
2022-11-25 20:04:34 +0200

    upd3

--- HW01cart.py
+++ HW01cart.py
@@ -1,4 +1,3 @@
-
 class Product:
 
     def __init__(self, name, price):
@@ -12,48 +11,32 @@ class Product:
         return f'product is: {self.name} and price is: {self.price}.'
 
     def total_price(self, quantity):
-        return f'total price will: {self.price * quantity}, if quantity {quantity}.'
-
-
-beers = Product('Beers', 100)
-mango = Product('Mango', 70)
-
-print(beers.show_product())
-print(beers.total_price(14))
-print(mango.show_product())
-print(mango.total_price(8))
+        return round(self.price * quantity, 2)
 
 
 class ShoppingCart:
     def __init__(self):
         self.products = []
         self.quantities = []
-        self.list_1 = []
-        self.total = 0
 
     def add_to_cart(self, product: Product, quantity):
         self.products.append(product)
         self.quantities.append(quantity)
         return self.products, self.quantities
-        # for i in range(len(self.products)):
-        #     print(f'prod list: {self.products[i]}')
-        # for i in range(len(self.quantities)):
-        #     print(f'quantities: {self.quantities[i]}')
 
     def total_price(self):
-        for i in range(len(self.products)):
-            print(self.products[i].price)
-            self.total = self.products[i].price * self.quantities[i]
-        print(f'T: {self.total}')
+        total = 0
+        for product, quantity in zip(self.products, self.quantities):
+            total += product.total_price(quantity)
+        return round(total, 2)
 
-        self.list_1.append(self.total)
-        print(f'In cart is {len(self.products)} items.')
-        return self.list_1
 
+beers = Product('Beers', 10.59)
+mango = Product('Mango', 36.55)
 
 cart_1 = ShoppingCart()
-cart_2 = ShoppingCart()
-cart_1.add_to_cart(beers, 1)
-cart_2.add_to_cart(mango, 24)
+
+cart_1.add_to_cart(beers, 0.7)
+cart_1.add_to_cart(mango, 4)
+
 print(cart_1.total_price())
-print(cart_2.total_price())
